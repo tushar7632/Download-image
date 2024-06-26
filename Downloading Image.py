d2add7322a2b4f050c0a50e915f93c550219a1ed
Tushar <singhtushar763200gmail.com>
2024-06-26 21:44:05 +0530

    first commit

---
+++ Downloading Image.py
@@ -0,0 +1,9 @@
+import requests
+url =input("Enter the image of url ")
+post =requests.get(url)
+
+#file handling....
+open('ramshyam.jpg','wb').write(post.content)
+print("FILE DOWNLOAD SUCCESSFULLY.....")
+
+'ramshyam.jpg'
